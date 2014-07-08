100927fe2d7de4a5654e1e045b35da33535de033
Mathieu THOMASSET <mathosset@gmail.com>
2014-07-08 18:29:12 +0200

    solution to stupid coach

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,16 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message.include? "?"
+    return "Silly question, get dressed and go to work !"
+  elsif your_message == "I am going to work right now SIR !"
+    return ""
+  else
+    return "I don't care son, get dressed and go to work !"
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours !
+
 end
 
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,10 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
+Puts "what do you want to tell to your coach ?"
+message = gets.chomp
 
+unless message == "I am going to work right now SIR !"
+  puts coach_answer(message)
+  Puts "what do you want to tell to your coach ?"
+  message = gets.chomp
+end
