5d48b4a511ccd027494498b77e9ace4131250ebe
Mathieu THOMASSET <mathosset@gmail.com>
2014-07-09 15:59:20 +0200

    solution to sorting exercise

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -7,8 +7,19 @@ begin
   name = gets
   name.chomp! if name
 
-  # TODO: Add the name we just gotto the students array
+  # TODO: Add the name we just got to the students array
+  students << name if name != ""
 
 end while name != ""
 
 # TODO: Call `wagon_sort` method and display the sorted student list
+
+students = wagon_sort(students)
+
+students_size = students.size
+
+puts "congratulations you have #{students_size} students : "
+
+first_part = students.first(students_size-1).join(", ")
+
+puts "#{first_part} and #{students.last}"
--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,4 @@
 def wagon_sort(students)
   # TODO: return a copy of students, sorted alphabetically
+  students.sort {|a, b| a.downcase <=> b.downcase}
 end
