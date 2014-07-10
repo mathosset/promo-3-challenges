6cad76226a6405fb26530f8d81ba9631bd2886c8
Mathieu THOMASSET <mathosset@gmail.com>
2014-07-10 12:22:15 +0200

    solution to block gym

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,12 +1,21 @@
 def tag(tag_name, attr = nil)
   #TODO:  Build HTML tags around  content given in the block
   #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  if attr == nil
+    "<#{tag_name}>#{yield}</#{tag_name}>"
+  else
+    "<#{tag_name} #{attr[0]}='#{attr[1]}'>#{yield}</#{tag_name}>"
+  end
 end
 
 def timer_for
   #TODO:  Return time taken to execute the given block
+  t = Time.now
+  yield
+  Time.now - t
 end
 
 def transform(element)
   #TODO:  Simply execute the given block on element
+  yield(element)
 end
