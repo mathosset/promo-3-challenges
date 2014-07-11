01a10ebb900afb6697e123140b58d6d1576c8b4e
Mathieu THOMASSET <mathosset@gmail.com>
2014-07-11 18:00:27 +0200

    solution to shopping cart

--- 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/cart.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/cart.rb
@@ -3,13 +3,20 @@
 
 def add_to_cart(cart, product)
   # TODO: you get a cart and a product. Add the product to the cart!
+  cart << product
 end
 
 def cart_to_s(cart)
   # TODO: you get a cart, return a coma-separated String list of the contained products
+  cart.map {|x| "#{x}, "}.join
 end
 
 def cart_total_price(cart, store_items)
   # TODO: you get a cart and the store items with their prices. Return the total
   # price of the shopping cart
+  total = 0
+  cart.each do |x|
+    total += store_items[x]
+  end
+  return total
 end
--- 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/store.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/store.rb
@@ -4,32 +4,33 @@
 
 def store_items
   # Here's what you should have in store:
-  #   yogurts : 2€
-  #   meat : 7€
-  #   vegetables: 5€
-  #   potatoes : 2€
-  #   rice : 1€
 
   # TODO: find the correct data structure to hold the shopping store items
   # instead of using nil below
-  store = nil
-
+  store = {
+    "yogurts" => 2,
+    "meat" => 7,
+    "vegetables" => 5,
+    "potatoes" => 2,
+    "rice" => 1
+  }
   return store
 end
 
 def is_product_in_store?(product)
   # TODO: Check whether product is present in the store items
+  store_items[product] != nil
 end
 
 def price_of_product(product)
   # TODO: Return the price of the given product, if it's in store. nil otherwise
+  return store[product] || nil
 end
 
 def store_items_to_s
   # TODO: Return a String with all store items in the following form:
   # - Meat: 7€
-  # - Vegetables: 5€
-  # ...
-  #
-  # TIP: add line breaks in your String with "\n"
+  # - Vegetables: 5€ ...
+  # TIP: add line breaks in your String wit h "\n"
+  store_items.map {|name, price| "- #{name} : #{price}€"}.join("\n")
 end
