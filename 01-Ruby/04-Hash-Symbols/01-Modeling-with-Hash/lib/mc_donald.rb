e194acf41d815aaa40df096730c596a424c89c67
Mathieu THOMASSET <mathosset@gmail.com>
2014-07-11 11:58:36 +0200

    solution to Mcdo calories counter

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,7 +1,38 @@
 def poor_calories_counter(burger, side, beverage)
   #TODO: return number of calories for this mcDonald order
+  ingredients = {
+    "Cheese Burger" => 290,
+    "Big Mac" => 300,
+    "Mc Bacon" => 400,
+    "Royal Cheese" => 130,
+    "French fries" => 130,
+    "Potatoes" => 130,
+    "Coca" => 160,
+    "Sprite" => 170
+  }
+  ingredients[burger] + ingredients[side] + ingredients[beverage]
 end
 
 def calories_counter(*orders)
   #TODO: return number of calories for a less constrained order
+   ingredients = {
+    "Cheese Burger" => 290,
+    "Big Mac" => 300,
+    "Mc Bacon" => 400,
+    "Royal Cheese" => 130,
+    "French fries" => 130,
+    "Potatoes" => 130,
+    "Coca" => 160,
+    "Sprite" => 170
+  }
+
+  ingredients["Happy Meal"] = poor_calories_counter("Cheese Burger", "French fries", "Coca")
+  ingredients["Best Of Big Mac"] = poor_calories_counter("Big Mac", "French fries", "Coca")
+  ingredients["Best Of Royal Cheese"] = poor_calories_counter("Royal Cheese", "Potatoes", "Sprite")
+
+  order = 0
+  orders.each do |k|
+    order += ingredients[k]
+  end
+  return order
 end
