ea22007d369cfa0c505db7296a5f3fce83b13c05
Mathieu THOMASSET <mathosset@gmail.com>
2014-07-11 18:30:50 +0200

    half solution to integer to roman

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -1,5 +1,22 @@
 def roman_to_integer(roman_string)
   #TODO: translate roman string to integer
+hash = {
+  "I" => 1,
+  "V" => 5,
+  "X" => 10,
+  "L" => 50,
+  "C" => 100
+}
+
+array_roman_string = roman_string.split('')
+
+total = 0
+
+array_roman_string.each do |l|
+  total += hash[l]
+end
+return total
+
 end
 
 # testing your program
