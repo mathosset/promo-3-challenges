91acb5be3f1c395c3bc3449d5cfa717d413951a6
Mathieu THOMASSET <mathosset@gmail.com>
2014-07-14 19:11:34 +0200

    beesong solution

--- 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
@@ -1,3 +1,20 @@
-def beersong(nb_at_start)
+nb_at_start = ARGV[0].to_i
+
+#def beersong(nb_at_start)
   #TODO: sing the song
-end
+  #ARGV.each do |nb_at_start|
+  if ARGV.empty?
+    puts "Pas d'argument"
+  else
+
+    for i in (1..nb_at_start)
+      if nb_at_start - i > 1
+        puts "#{nb_at_start-i} bottles of beer on the wall, #{nb_at_start-i } bottles of beer! \n"
+        puts "Take one down, pass it around, #{nb_at_start-i-1} bottles of beer on the wall!"
+      else
+        puts "1 bottle of beer on the wall, 1 bottle of beer! \n"
+        puts "Take one down, pass it around, no more bottles of beer on the wall!"
+      end
+    end
+
+  end
