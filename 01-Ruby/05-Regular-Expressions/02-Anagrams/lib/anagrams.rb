438845d1b2862061fb787c5786999d157683f4d6
Mathieu THOMASSET <mathosset@gmail.com>
2014-07-15 14:53:07 +0200

    anagrams solution

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,8 +1,25 @@
 def anagrams?(a_string, another_string)
   #TODO: implement the obvious method to test if two words are anagrams
+
+  if a_string.downcase.scan(/\w/).sort == another_string.downcase.scan(/\w/).sort
+    return true
+  else
+    return false
+  end
+
 end
 
 def anagrams_on_steroids?(a_string, another_string)
   #TODO: implement the improved method
+
+  hash_a_string = Hash.new
+  array_a_string = a_string.downcase.scan(/\w/)
+  array_a_string.each { |letter| hash_a_string[letter] = array_a_string.count(letter) }
+
+  hash_another_string = Hash.new
+  array_another_string = another_string.downcase.scan(/\w/)
+  array_another_string.each { |letter| hash_another_string[letter] = array_another_string.count(letter) }
+
+  hash_a_string == hash_another_string
+
 end
-  
