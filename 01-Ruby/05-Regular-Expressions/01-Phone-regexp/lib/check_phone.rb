217f0094182bdc1ed172038e382d9507e782790d
Mathieu THOMASSET <mathosset@gmail.com>
2014-07-15 11:57:42 +0200

    phone number solution

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,12 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+
+  phone = phone_number !~ /^(0|\+33)(\s|\W|)[1-9](\s|\W|)(\d{2})(\s|\W|)(\d{2})(\s|\W|)(\d{2})(\s|\W|)(\d{2})$/
+
+  if phone == true
+    false
+  else
+    true
+  end
+
 end
