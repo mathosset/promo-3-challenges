746ff971de3ea4c963f7fa415f9f68f4e06126e0
Mathieu THOMASSET <mathosset@gmail.com>
2014-07-16 21:58:54 +0200

    partial solution to Public Interface

--- 02-OOP/04-Public-Interface/lib/account.rb
+++ 02-OOP/04-Public-Interface/lib/account.rb
@@ -11,6 +11,7 @@ class BankAccount
   # - you can print transactions only with a password
   # - you can withdraw or deposit money
   # - You can see the balance of the account (through the position variable)
+  attr_reader :name, :position
 
   MIN_DEPOSIT =  100
 
@@ -26,33 +27,48 @@ class BankAccount
 
   def withdraw(amount)
     # TODO: Call add_transaction with the right argument
+    add_transaction(-amount)
     # TODO: returns a string with a message
+    return "You withdrawn #{amount} €."
   end
 
   def deposit(amount)
     # TODO: Call add_transaction with the right argument
+    add_transaction(amount)
     # TODO: returns a string with a message
+    return "You deposit #{amount} €."
   end
 
   def transactions_history(args = {})
     # TODO: Check if there is a password and if so if it is correct
+    if args[:password] == @password
     # TODO: return a string displaying the transactions, BUT NOT return the transaction array !
+      @transactions.map {|transaction| "#{transaction.to_s} \n"}
+    elsif args[:password] == nil
+      "no password given"
+    else
+      "wrong password"
+    end
   end
 
   def iban
     # TODO: Hide the middle of the IBAN like FR14**************606 and return it
+    return "#{@iban[0..3]}**************#{@iban.split('').last(3).join}"
   end
 
   def to_s
     # Method used when printing account object as string (also used for string interpolation)
     # TODO: Displays the account owner, the hidden iban and the position of the account
+    return "Account owner: #{name} #{iban} + #{@position}"
   end
 
   private
 
   def add_transaction(amount)
     # TODO: add the amount in the transactions array
+    @transactions << amount
     # TODO: update the current position (which represents the balance of the account)
+    @position += amount
   end
 
 end
--- 02-OOP/04-Public-Interface/lib/transaction.rb
+++ 02-OOP/04-Public-Interface/lib/transaction.rb
@@ -2,9 +2,12 @@
 class Transaction
   def initialize(amount)
     # Initialize amount and date of transaction
+    @amount = amount
+    @date_of_transaction = Time.now
   end
 
   def to_s
     # Nicely print transaction infos using Time#strftime.
+
   end
 end
