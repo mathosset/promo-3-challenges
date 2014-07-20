5dbee7697d206c03ad207c5a04c0616a47210745
Mathieu THOMASSET <mathosset@gmail.com>
2014-07-20 22:20:29 +0200

    Controller and view of Cookbook V0

--- 02-OOP/03-Cookbook/01-Cookbook-v0/lib/controller.rb
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/lib/controller.rb
@@ -1,18 +1,26 @@
+require_relative 'cookbook'
+require_relative 'view'
+require_relative 'recipe'
+
 class Controller
 
   def initialize(cookbook)
     @cookbook = cookbook
+    @view = View.new
 
   end
 
   def list
+    @view.list(@cookbook.recipes)
 
   end
 
-  def display
+  def create
+    @cookbook.add_recipe(Recipe.new(@view.add_recipe[0], @view.add_recipe[1]))
   end
 
   def destroy
+    @cookbook.remove_recipe(@view.destroy_recipe)
   end
 
 
---
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/lib/view.rb
@@ -0,0 +1,22 @@
+require_relative 'cookbook'
+require_relative 'controller'
+require_relative 'router'
+
+
+class View
+
+  def list(recipes)
+    recipes.each_index do |recipe, index|
+      puts "#{index} - #{recipe}"
+  end
+
+  def add_recipe(name, description)
+    puts "what is the name and the description (separate by +) of the new recipe:"
+    return gets.chomp.split("+")
+  end
+
+  def destroy_recipe(index)
+    puts "which recipe would you like to remove:"
+    return gest.chomp.to_i
+
+end
