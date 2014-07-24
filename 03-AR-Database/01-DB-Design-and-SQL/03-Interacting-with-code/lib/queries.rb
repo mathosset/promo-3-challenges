289b4bc23c222e0ae10e03245e0a885694ee4a2d
Mathieu THOMASSET <mathosset@gmail.com>
2014-07-24 15:44:30 +0200

    interacting with code

--- 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
@@ -6,16 +6,24 @@ db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db, table_name)
   #TODO: count number of rows in table table_name
+  stm = db.execute "SELECT * FROM #{table_name}"
+  stm.count
 end
 
 def sorted_artists(db)
   #TODO: return array of artists' names sorted alphabetically
+  stm = db.execute "SELECT name FROM artist ORDER BY name"
+  stm.flatten
 end
 
 def love_tracks(db)
   #TODO: return array of love songs
+  stm = db.execute "SELECT name FROM track WHERE name LIKE '%love%'"
+  stm.flatten
 end
 
 def long_tracks(db, min_length)
   #TODO: return tracks verifying: duration > min_length (minutes)
+  stm = db.execute "SELECT name FROM track WHERE milliseconds > #{min_length * 60000}"
+  stm.flatten
 end
