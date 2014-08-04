c4a3c2d5be36285bdabfabfaeb0ef6cdbbbd07f5
Mathieu THOMASSET <mathosset@gmail.com>
2014-08-04 16:25:07 +0200

    JS 02 EVENTS ANIMATION

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,15 @@
   * TODO: add your code here!
   *
 */
+$('#message-wrapper').css("top", "-45px");
+
+$('#message-wrapper').delay(1000).animate({top: "0px"}, 2000 );
+
+$('#message-wrapper').click(function() {
+  $('#message').hide()
+});
+
+
+
+
+
